b300b8837a170cc107f3d21d4628fe5fe5ddf0eb
mrpillowofthefan <94277008+mrpillowofthefan@users.noreply.github.com>
2021-12-01 18:47:36 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+var mouseevent="empty";
+var firstpositionofx, firstpositionofy;
+var color="#718a3d";
+canvas.addEventListener("mousedown",mymousedown);
+function mymousedown(e){
+    mouseevent="mousedown";
+}
+canvas.addEventListener("mouseup", mymouseup);
+function mymouseup(e){
+    mouseevent="mouseup";
+}
+canvas.addEventListener("mouseleave", mymouseleave);
+    function mymouseleave(e){
+    mouseevent="mouseleave";
+}
+canvas.addEventListener("mousemove", mymousemove);
+function mymousemove(e){
+    currentpositionofx=e.clientX-canvas.offsetLeft;
+    currentpositionofy=e.clientY-canvas.offsetTop;
+    if(mouseevent="mousedown"){
+        ctx.beginPath();
+        ctx.strokeStyle=color;
+        ctx.lineWidth=2;
+        ctx.arc(currentpositionofx, currentpositionofy, 30, 0, 2 * Math.PI)
+        ctx.stroke();
+    }
+    firstpositionofx=currentpositionofx;
+    firstpositionofy=currentpositionofy;
+}
